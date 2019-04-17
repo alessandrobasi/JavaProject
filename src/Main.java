74f6143fedc1312c2eb84f059c602b6a8e2743ab
alessandrobasi <alessandrobasi@live.it>
2019-04-17 23:56:52 +0200

    comments added

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,8 @@ public class Main {
 
         int Utenti = 99;
 
+        // Program Arguments
+        // -U [Numero] : Utenti da mettere nell'arena
         for(int i = 0; i< args.length; i++){
             if(args[i].startsWith("-")){
                 switch (args[i]){
@@ -14,25 +16,30 @@ public class Main {
                             Utenti = Integer.parseInt(args[i+1]);
                         }
                         catch (NumberFormatException e){
-                            ;
+                            Utenti = 99;
                         }
                         break;
+                    case "-?":
+                        System.out.println("Argomenti disponibili\n"+"-? : Mostra questo help\n"+"-U : Utenti da mettere nell'arena\n");
+                        break;
                     default:
-                        continue;
+                        break;
 
                 }
-            }
-            else {
-                continue;
             }
 
         }
 
 
+        // Creazione arena
         Arena round = new Arena();
 
+        // Aggiunta utenti
+        // Numero di default: 99
+        // Numero variabile:  Utenti
         round.bulk_add(Utenti);
 
+        //
         System.out.println( round.get_size() );
 
 
